d05e7c88e2f27fa3885f263acbee4d839f56eace
Rene Hangstrup Møller <rhmoller@gmail.com>
2019-03-05 20:59:28 +0100

    feat: add more elaborate test of wat tag

--- src/wat-tag.test.ts
+++ src/wat-tag.test.ts
@@ -1,6 +1,7 @@
 import {wat} from "./wasm-util";
 
 describe("wat-tag", () => {
+
     it("Compiles an empty module", async (done) => {
         const module = await wat`(module)`;
         const instance = WebAssembly.instantiate(module);
@@ -24,4 +25,43 @@ describe("wat-tag", () => {
         expect(instance.exports.square(3)).toBe(9);
         done();
     });
+
+
+    it("counts to 5" , async (done) => {
+        const module = await wat`
+            (module
+                ;; import trace function that accepts a single i32 argument
+                (func $trace (import "js" "trace") (param i32))
+                
+                ;; prints numbers from 1 to $max
+                (func (export "countTo") (param $max i32)
+                    ;; define variable $c and initialize it to 0
+                    (local $c i32)
+                    (set_local $c (i32.const 0))
+            
+                    ;; start a loop
+                    (loop $counting
+                        ;; increment $c by 1
+                        (set_local $c (i32.add (get_local $c) (i32.const 1)))
+                        ;; log current value of $c
+                        (call $trace (get_local $c))
+                        ;; repeat loop if $c is not equal to $max
+                        (br_if $counting (i32.ne (get_local $max) (get_local $c)))
+                    )
+                )
+            )
+        `;
+
+        const log: Array<number> = [];
+        const instance = await WebAssembly.instantiate(module, {
+            "js": {
+                trace: (i: any) => log.push(i)
+            }
+        });
+
+        instance.exports.countTo(5);
+        expect(log).toEqual([1, 2, 3, 4, 5]);
+        done();
+    });
+
 });
